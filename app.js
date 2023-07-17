1c514045a3dc4492e94e73172503a615c1405231
Thanchote Li <thanachote.mmn@gmail.com>
2023-07-17 12:36:25 +0700

    Update the style and the js code

--- app.js
+++ app.js
@@ -2,34 +2,58 @@ player1Button = document.querySelector("#p1increase");
 player2Button = document.querySelector("#p2increase");
 player1PointDisplay = document.querySelector("#p1point");
 player2PointDisplay = document.querySelector("#p2point");
-reset = document.querySelector("#reset");
+resetButton = document.querySelector("#reset");
 let player1Point = 0;
 let player2Point = 0;
+let isPlayer1Win = false;
+let isPlayer2Win = false;
 
-// var select = document.getElementById('winpoint');
-// v   ar currentOpt = select.option[select.selectedIndex];
-
-// winPoint = document.querySelector('#winpoint option:checked').value;
+let isGameEnd = false; // will be true when one of the player reach the setted win point, then the score must be reset to continue
 
 player1Button.addEventListener("click", () => {
+    isPlayer1Win = checkWin(player1Point);
     // console.log("Player 1");
-    player1Point++;
+    if (isPlayer1Win || isPlayer2Win ? player1Point=player1Point : player1Point++);
     // console.log(player1Point);
     // const winPoint = document.querySelector('#winpoint option:checked').value;
     // console.log(winPoint);
     player1PointDisplay.innerText = `${player1Point}`;
-    isThisPlayerWin = checkWin(player1Point);
-    if (isThisPlayerWin) {
+
+    isPlayer1Win = checkWin(player1Point);
+    if (isPlayer1Win) {
         // console.log("Win")
-        player1PointDisplay.classList.add("winner")
-        player2PointDisplay.classList.add("loser")
+        player1PointDisplay.classList.add("winner");
+        player2PointDisplay.classList.add("loser");
+        resetButton.classList.add("reset_dominate");
     }
 });
 // ref: element.classList.contains(className);
 
 player2Button.addEventListener("click", () => {
     // console.log("Player 2");
+    isPlayer2Win = checkWin(player2Point);
+    if (isPlayer2Win || isPlayer1Win ? player2Point=player2Point : player2Point++);
+    player2PointDisplay.innerText = `${player2Point}`;
+
+    isPlayer2Win = checkWin(player2Point);
+    if (isPlayer2Win) {
+        player1PointDisplay.classList.add("loser");
+        player2PointDisplay.classList.add("winner");
+        resetButton.classList.add("reset_dominate");
+    }
 });
+
+resetButton.addEventListener('click', () => {
+    player1PointDisplay.innerText = 0;
+    player2PointDisplay.innerText = 0;
+    player1PointDisplay.className = "";
+    player2PointDisplay.className = "";
+    resetButton.classList.remove("reset_dominate");
+    isPlayer1Win = false;
+    isPlayer2Win = false;
+    player1Point = 0;
+    player2Point = 0;
+})
 
 function checkWin(playerPoint) {
     const currentWinPoint = document.querySelector('#winpoint option:checked').value;
